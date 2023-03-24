cac6c27dbe59388891f47b7bf325e638f08caf4d
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2023-03-24 20:21:52 +0530

    Initial commit

---
+++ 24-03-2023/index.py
@@ -0,0 +1,34 @@
+# Number
+length = 20
+breadth = 10.3
+
+area = length * breadth
+
+# print(area)
+
+# String
+name = "Abhilash"
+# print(name)
+
+# Boolean
+is_male = True
+# print(is_male)
+
+
+# Function - reusable piece of code
+# Function definition - parameters
+def area_of_triangle(base, height):
+    print(0.5 * base * height)
+
+
+# Calling a function - using/consuming a function
+# arguments
+area_of_triangle(2, 4)
+area_of_triangle(4, 8)
+
+
+def greet(name):
+    print("Hey there,", name)
+
+
+greet("Abhilash")
